69dba9b27a94d957a33ed336abd9e1d3312625fe
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-20 19:01:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,3 @@ end
 fido = Dog.new
 snoopy = Dog.new
 lassie = Dog.new
-
-class Person
-  def first_name(name)
-    name = "#{name}"
-  end
-end
-
-adele_goldber = Person.new
-alan_kay  = Person.new
